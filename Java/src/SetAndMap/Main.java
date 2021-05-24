9d669ed58b131baff7585e5dc2b3e4bb0adcbb74
hzhq1255 <hzhq1255@163.com>
2021-05-25 00:01:23 +0800

    init

---
+++ Java/src/SetAndMap/Main.java
@@ -0,0 +1,20 @@
+package SetAndMap;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @Author: hzhq1255
+ * @Mail: hzhq1255@163.com
+ * @Date: 2021/5/24 23:46
+ * @Desc:
+ */
+public class Main {
+    public static void main(String[] args){
+        // 散列类集合 HashSet
+        Set<Integer> hashSet = new HashSet<>();
+        // 链式散列类集合 LinkedSet
+        Set<Integer> linkedSet = new LinkedHashSet<>();
+    }
+}
---
+++ Java/src/SetAndMap/MySet.java
@@ -0,0 +1,10 @@
+package SetAndMap;
+
+/**
+ * @Author: hzhq1255
+ * @Mail: hzhq1255@163.com
+ * @Date: 2021/5/24 23:45
+ * @Desc:
+ */
+public class MySet {
+}
