5afbefdd183602c70ffaf7e7a02cce38ffdc86d3
hzhq1255 <hzhq1255@163.com>
2021-09-11 09:12:55 +0800

    add my first shell script

---
+++ Shell/hello.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# /bin/bash 指定shell解释器为bash
+# "#!" 告诉系统该以什么解释器执行
+
+echo "Hello World!" 
+
+# echo 向窗口输出脚本
+# 执行脚本需要赋予 脚本执行权限
+# chmod +x ./hello.sh
+# ./hello.sh 执行脚本
+# 没有加 ./ 目录的 回去 PATH 变量找这个脚本
+# 也可以直接用解释器直接执行脚本
+# /bin/sh test.sh
+# /bin/bash test.sh
