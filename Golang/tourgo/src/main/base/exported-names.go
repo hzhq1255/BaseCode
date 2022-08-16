c60a9e36855413d2dac4b4271a79cbea668a45dd
hzhq1255 <hzhq1255@163.com>
2022-08-16 22:14:13 +0800

    add module packages

---
+++ Golang/tourgo/src/main/base/exported-names.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// go
+	// variable and method first upper letter is public
+	// such as math.Pi
+	// first lower letter is private
+	// such as math.pi
+	// fmt.Println(math.pi)
+	fmt.Println(math.Pi)
+}
