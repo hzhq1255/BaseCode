f2195a310fc6821a97c8059dbba312f03840603f
hzhq1255 <hzhq1255@163.com>
2022-08-16 12:33:30 +0800

    add  exercise-errors.go reader.go

---
+++ Golang/tourgo/src/main/exercise-errors.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	return math.Sqrt(x), nil
+}
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func (e *ErrNegativeSqrt) Test() string {
+	return "hello"
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
