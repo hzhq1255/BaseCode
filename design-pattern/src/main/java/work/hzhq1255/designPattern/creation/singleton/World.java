117c0fd05cecf0d54c1041f121be176810dfbd7d
hzhq1255 <hzhq1255@163.com>
2023-04-02 22:39:37 +0800

    study: singleton pattern

---
+++ design-pattern/src/main/java/work/hzhq1255/designPattern/creation/singleton/World.java
@@ -0,0 +1,18 @@
+package work.hzhq1255.designPattern.creation.singleton;
+
+/**
+ * @author hzhq
+ * @version 1.0
+ * @since 2023/4/2 下午10:33
+ * 通过枚举实现单例
+ */
+public enum World {
+    // 唯一枚举
+    INSTANCE;
+
+    private Singleton singleton = Singleton.getInstance();
+
+    public Singleton getSingleton() {
+        return singleton;
+    }
+}
