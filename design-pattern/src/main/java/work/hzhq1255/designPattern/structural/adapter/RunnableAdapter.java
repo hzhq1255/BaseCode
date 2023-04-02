96ae3f01ff3f7c30eedae824d2ac16b5d887993e
hzhq1255 <hzhq1255@163.com>
2023-04-02 23:55:39 +0800

    study: adapter pattern

---
+++ design-pattern/src/main/java/work/hzhq1255/designPattern/structural/adapter/RunnableAdapter.java
@@ -0,0 +1,28 @@
+package work.hzhq1255.designPattern.structural.adapter;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author hzhq
+ * @version 1.0
+ * @since 2023/4/2 下午11:40
+ */
+public class RunnableAdapter implements Runnable {
+
+    // 引用待转换接口
+    private Callable<?> callable;
+
+    public RunnableAdapter(Callable<?> callable) {
+        this.callable = callable;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // 调用委托接口进行转换
+            callable.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
